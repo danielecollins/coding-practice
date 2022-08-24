a011b5adeb54048fa96348ee781c337570b679fc
danielecollins <57637750+danielecollins@users.noreply.github.com>
2022-08-24 14:50:57 -0600

    C# coding practice

---
+++ ct_q2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Platformer
+{
+    private int numTiles;
+    private int tile;
+    private List<int> tileList = new List<int>();
+    private int listPos;
+
+    public Platformer(int numberOfTiles, int position)
+    {
+        numTiles = numberOfTiles;
+        tile = position;
+        listPos = position;
+
+        for (int i = 0; i < numberOfTiles; i++)
+        {
+            tileList.Add(i);
+        }
+    }
+
+    public void JumpLeft()
+    {
+        listPos = tileList.IndexOf(tile) - 2;
+        tileList.RemoveAt(tileList.IndexOf(tile));
+        tile = tileList.ElementAt(listPos);
+    }
+
+    public void JumpRight()
+    {
+        listPos = tileList.IndexOf(tile) + 2;
+        tileList.RemoveAt(tileList.IndexOf(tile));
+        tile = tileList.ElementAt(listPos - 1);
+    }
+
+    public int Position()
+    {
+        return tile;
+    }
+
+    public static void Main(string[] args)
+    {
+        Platformer platformer = new Platformer(6, 3);
+        Console.WriteLine(platformer.Position()); // should print 3
+
+        platformer.JumpLeft();
+        Console.WriteLine(platformer.Position()); // should print 1
+
+        platformer.JumpRight();
+        Console.WriteLine(platformer.Position()); // should print 4
+    }
+}
---
+++ ct_q3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LanguageStudent 
+{ 
+    public List<string> _languages = new List<string>();
+
+    public List<string> Languages
+    {
+        get
+        {
+            return _languages;
+        }  
+    }
+
+    public void AddLanguage(string Language) 
+    {
+        Languages.Add(Language);
+    }
+}
+
+public class LanguageTeacher : LanguageStudent 
+{
+    public bool Teach(LanguageStudent student, string language) 
+    {
+        foreach (var lang in this.Languages)
+        {
+            if (language == lang)
+            {
+                student.Languages.Add(language);
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        LanguageTeacher teacher = new LanguageTeacher();
+        teacher.AddLanguage("English");
+
+        LanguageStudent student = new LanguageStudent();
+        teacher.Teach(student, "English");
+
+        foreach (var language in student.Languages)
+            Console.WriteLine(language);
+    }
+}
